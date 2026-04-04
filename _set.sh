c5414923d1e1396513604fc385dbd6d28c2afe75
qing <1776177662@qq.com>
2026-04-04 08:23:02 +0000

    ^-^

--- _set.sh
+++ _set.sh
@@ -3,5 +3,3 @@ cd "$(dirname "$0")" || exit
 python3 -m venv .env
 source .env/bin/activate
 pip install -r requirements.txt -i https://mirrors.aliyun.com/pypi/simple/
-#source gen_cert.sh
-#python3 upd.py --init-db
